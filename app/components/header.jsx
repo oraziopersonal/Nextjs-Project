d6ba718f632bb060f9dd5683fa1867ef22634631
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-21 12:44:29 +0200

    Create header.jsx
    add header portfolio component

---
+++ app/components/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Hederer() {
+return (
+  <header>
+    <h1>MY PORTFOLIO</h1>
+      <p>IA DEVELOPER</p>
+        <nav>
+          <ul>
+            <li><a href="/home.html"></li>
+            <li><a href="/chisono.html"></li>
+            <li><a href="/contatti.html"></li>
+          </ul>
+        <nav>
+</header>
+)
+};
+
+export default Header;

7f52f61444748c0f38230aaa35260d9a7f929327
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-21 12:28:02 +0200

    Create Footer.jsx
    footer component nextjs

---
+++ app/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  <>
+    <footer className="footer">
+      <div className="footer-block">
+        <p className="footer-p">
+          &copy; {new Date().getFullYear()} MY POrtfolio castellano orazio        </p>
+      </div>
+    </footer>
+    
+  </>
+}
